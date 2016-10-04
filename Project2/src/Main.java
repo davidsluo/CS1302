70bc3d2b1663bfaa27fc9ae484972dc0bfad8997
David Luo <davidl2381@gmail.com>
2016-10-04 13:01:57 -0400

    Re-added catch all try catch loop. Added welcome message. Added clear to run.sh before running main program.

--- Project2/src/Main.java
+++ Project2/src/Main.java
@@ -8,107 +8,111 @@ import java.util.Scanner;
  */
 public class Main {
 
-    private static List<Order> orders = new ArrayList<>();
-    private static Scanner scanner = new Scanner(System.in);
+    private static List<Order> orders  = new ArrayList<>();
+    private static Scanner     scanner = new Scanner(System.in);
 
 
     public static void main(String[] args) {
 
         boolean cont;
-//        try {
-//        } catch (Exception | Error e) {
-//            if (getYesNo("Something went wrong. Exit? (Y/N)"))
-//                cont = false;
-//        }
+        System.out.println("Welcome to <Pizza Place>.");
+        while (true) {
+            try {
 
-        do {
-            // TODO: 10/4/2016 Loop this for new orders
-            cont = true;
-            Order order = new Order();
-            while (cont) {
-                Order.printMenu();
-                System.out.println(Order.getOptionsMenu());
-                switch (scanner.next()) {
-                    case "1":
-                        do {
-                            Pizza.Topping topping = null;
-                            Pizza.Size size = null;
-                            int pQuantity;
-                            // Map to array instead?
-                            // TODO: 10/3/2016 Make this prettier
-                            switch (getStrInput(Pizza.getSizeSelectionMenu(), "1", "2", "3")) {
-                                case "1":
-                                    size = Pizza.Size.SMALL;
-                                    break;
-                                case "2":
-                                    size = Pizza.Size.MEDIUM;
-                                    break;
-                                case "3":
-                                    size = Pizza.Size.LARGE;
-                                    break;
-                            }
-                            switch (getStrInput(Pizza.getToppingSelectionMenu(), "1", "2", "3")) {
-                                case "1":
-                                    topping = Pizza.Topping.VEGETARIAN;
-                                    break;
-                                case "2":
-                                    topping = Pizza.Topping.CHEESE;
-                                    break;
-                                case "3":
-                                    topping = Pizza.Topping.PEPPERONI;
-                                    break;
-                            }
+                do {
+                    // TODO: 10/4/2016 Loop this for new orders
+                    cont = true;
+                    Order order = new Order();
+                    while (cont) {
+                        Order.printMenu();
+                        System.out.println(Order.getOptionsMenu());
+                        switch (scanner.next()) {
+                            case "1":
+                                do {
+                                    Pizza.Topping topping = null;
+                                    Pizza.Size    size    = null;
+                                    int           pQuantity;
+                                    // Map to array instead?
+                                    // TODO: 10/3/2016 Make this prettier
+                                    switch (getStrInput(Pizza.getSizeSelectionMenu(), "1", "2", "3")) {
+                                        case "1":
+                                            size = Pizza.Size.SMALL;
+                                            break;
+                                        case "2":
+                                            size = Pizza.Size.MEDIUM;
+                                            break;
+                                        case "3":
+                                            size = Pizza.Size.LARGE;
+                                            break;
+                                    }
+                                    switch (getStrInput(Pizza.getToppingSelectionMenu(), "1", "2", "3")) {
+                                        case "1":
+                                            topping = Pizza.Topping.VEGETARIAN;
+                                            break;
+                                        case "2":
+                                            topping = Pizza.Topping.CHEESE;
+                                            break;
+                                        case "3":
+                                            topping = Pizza.Topping.PEPPERONI;
+                                            break;
+                                    }
 
-                            pQuantity = getIntInput(Item.getQuantityMenu());
+                                    pQuantity = getIntInput(Item.getQuantityMenu());
 
-                            assert topping != null;
-                            assert size != null;
-                            assert pQuantity != 0;
+                                    assert topping != null;
+                                    assert size != null;
+                                    assert pQuantity != 0;
 
-                            order.addItem(new Pizza(topping, size, pQuantity));
-                            order.printInvoice();
-                        } while (getYesNo("Order another pizza? (Y/N)"));
-                        break;
-                    case "2":
-                        do {
-                            Appetizer.AppetizerType type = null;
-                            int aQuantity;
+                                    order.addItem(new Pizza(topping, size, pQuantity));
+                                    order.printInvoice();
+                                } while (getYesNo("Order another pizza? (Y/N)"));
+                                break;
+                            case "2":
+                                do {
+                                    Appetizer.AppetizerType type = null;
+                                    int                     aQuantity;
 
-                            switch (getStrInput(Appetizer.getSelectionMenu(), "1", "2")) {
-                                case "1":
-                                    type = Appetizer.AppetizerType.SOUP;
-                                    break;
-                                case "2":
-                                    type = Appetizer.AppetizerType.SALAD;
-                                    break;
-                            }
+                                    switch (getStrInput(Appetizer.getSelectionMenu(), "1", "2")) {
+                                        case "1":
+                                            type = Appetizer.AppetizerType.SOUP;
+                                            break;
+                                        case "2":
+                                            type = Appetizer.AppetizerType.SALAD;
+                                            break;
+                                    }
 
-                            aQuantity = getIntInput(Item.getQuantityMenu());
+                                    aQuantity = getIntInput(Item.getQuantityMenu());
 
-                            assert type != null;
-                            assert aQuantity != 0;
-                            order.addItem(new Appetizer(type, aQuantity));
-                            order.printInvoice();
-                        } while (getYesNo("Order another appetizer? (Y/N)"));
-                        break;
-                    case "3":
-                        do {
-                            int dQuantity = getIntInput(Item.getQuantityMenu());
+                                    assert type != null;
+                                    assert aQuantity != 0;
+                                    order.addItem(new Appetizer(type, aQuantity));
+                                    order.printInvoice();
+                                } while (getYesNo("Order another appetizer? (Y/N)"));
+                                break;
+                            case "3":
+                                do {
+                                    int dQuantity = getIntInput(Item.getQuantityMenu());
 
-                            assert dQuantity != 0;
-                            order.addItem(new Drink(dQuantity));
-                            order.printInvoice();
-                        } while (getYesNo("Order another drink? (Y/N)"));
-                        break;
-                    default:
-                        cont = false;
-                        break;
-                }
+                                    assert dQuantity != 0;
+                                    order.addItem(new Drink(dQuantity));
+                                    order.printInvoice();
+                                } while (getYesNo("Order another drink? (Y/N)"));
+                                break;
+                            default:
+                                cont = false;
+                                break;
+                        }
+                    }
+                    order.printInvoice();
+                    orders.add(order);
+
+                } while (getStrInput("Order finished. Enter N for new customer or E to exit.", "n", "e").equals("n"));
+                break;
+            } catch (Exception | Error e) {
+                if (getYesNo("Something went wrong. Exit? (Y/N)"))
+                    break;
             }
-            order.printInvoice();
-            orders.add(order);
-
-        } while (getStrInput("Order finished. Enter N for new customer or E to exit.", "n", "e").equals("n"));
+        }
 
         printSummary();
 
@@ -116,9 +120,9 @@ public class Main {
 
     private static void printSummary() {
 
-        final String ORDER_TITLE = "Order %d\n";
+        final String ORDER_TITLE  = "Order %d\n";
         final String ORDER_HEADER = "%1$-25s%2$-10s$%3$4s\n";
-        final String ORDER_ITEM = "%1$-25s%2$-10s$%3$4.2f\n";
+        final String ORDER_ITEM   = "%1$-25s%2$-10s$%3$4.2f\n";
 
         System.out.println("Session Summary");
         for (int i = 0; i < orders.size(); i++) {
--- Project2/src/run.sh
+++ Project2/src/run.sh
@@ -1,2 +1,3 @@
 # Compile using the makefile before running this.
+clear
 java Main
