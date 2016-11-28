e82742e149644250af0de3ff7e8537aedba7d8e6
David Luo <davidl2381@gmail.com>
2016-11-28 18:43:44 -0500

    merge demo

--- Project4/src/Demo.java
+++ Project4/src/Demo.java
@@ -1,4 +1,5 @@
 import LinkedList.SortedDoublyLinkedList;
+import com.sun.org.apache.xpath.internal.SourceTree;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -11,17 +12,19 @@ public class Demo {
 
 
     public static void main(String[] args) {
+
+        Student benson = new Student("Benson", "Chau", 1, convertToDate(3, 13, 1978), "UGA");
+        Student frank = new Student("Frank", "Smith", 2, convertToDate(1, 1, 2842), "Harvard");
+        Student hawking = new Student("Stephen", "Hawking", 3, convertToDate(3, 2, 2981), "MIT");
+
+        Person grandpa = new Person("Kyle", "Grandpa", 238, convertToDate(1, 1, 1997));
+        Person rick = new Person("Rick", "Astley", 3841, convertToDate(2, 6, 1966));
+
         {
             // studentList is a prefix of personList when sorted by ID
             SortedDoublyLinkedList<Person> peopleList = new SortedDoublyLinkedList<>();
             SortedDoublyLinkedList<Student> studentList = new SortedDoublyLinkedList<>();
 
-            Student benson = new Student("Benson", "Chau", 1, convertToDate(3, 13, 1978), "UGA");
-            Student frank = new Student("Frank", "Smith", 2, convertToDate(1, 1, 2842), "Harvard");
-            Student hawking = new Student("Stephen", "Hawking", 3, convertToDate(3, 2, 2981), "MIT");
-
-            Person grandpa = new Person("Kyle", "Grandpa", 238, convertToDate(1, 1, 1997));
-            Person rick = new Person("Rick", "Astley", 3841, convertToDate(2, 6, 1966));
 
             // Regular unsorted arrays for convenience.
             Person[] people = {
@@ -51,6 +54,27 @@ public class Demo {
 
             System.out.println("Students prefix of People?");
             System.out.println(studentList.isPrefix(peopleList));
+        }
+        {
+            SortedDoublyLinkedList<Student> onlyStudents = new SortedDoublyLinkedList<>();
+            SortedDoublyLinkedList<Person> onlyPeople = new SortedDoublyLinkedList<>();
+
+            onlyStudents.add(benson);
+            onlyStudents.add(frank);
+            onlyStudents.add(hawking);
+
+            onlyPeople.add(grandpa);
+            onlyPeople.add(rick);
+
+            System.out.println("Just People");
+            onlyPeople.printList();
+            System.out.println("Just Students");
+            onlyStudents.printList();
+
+            SortedDoublyLinkedList<Person> merged = onlyPeople.merge(onlyStudents);
+
+            System.out.println("Merged");
+            merged.printList();
         }
     }
 
